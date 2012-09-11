7ef09969e520fa02b93a830f630a4a788ab2f526
Quintin Smith <quintinsmith@gmail.com>
2012-09-11 16:32:48 -0600

    character idea
    Signed-off-by: Quintin Smith <quintinsmith@gmail.com>

--- characters/characters/main.cpp
+++ characters/characters/main.cpp
@@ -8,11 +8,41 @@
 
 #include <iostream>
 
+using namespace std;
+
 int main(int argc, const char * argv[])
 {
 
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    const unsigned int MAX = 20;
+    char ch = '7';
+    int add;
+    
+    char cha[MAX];
+    unsigned int total;
+   
+    cout << ch - 48 << endl;
+    
+    for(int i=0; i<10; i++)
+    {
+        char one = '1';
+        cha[i] = (i + 48);
+        total += cha[i];
+        cout << cha[i] << endl;
+      
+    }
+    cout << total << endl;
+    
+    add = cha[0] + cha[1];
+    cout << cha[1] - 48 + cha[1] - 48 << endl;
+    
+    
+    
+    
+    
+    
+    
+    
+    
     return 0;
 }
 
